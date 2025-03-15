5cd0402050bb3d26c2fb72f4cbb9e621172da2e0
Amir <0204.amir@gmail.com>
2025-03-15 19:33:32 +0300

    Edit extension info

--- dynamic/new/web/app.py
+++ dynamic/new/web/app.py
@@ -1,4 +1,5 @@
 import subprocess
+import requests as r
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
@@ -23,6 +24,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about_one.html')
+
+@app.route('/about/<ext>')
+def about_ext(ext):
+    resp = r.get(f"https://marketplace.visualstudio.com/items?itemName={ext}")
+    data = resp.text
+    try:
+        name = data.split("ux-item-name")[2].split(">")[1].split("<")[0]
+    except:
+        name = "Not found"
+    try: 
+        author = data.split("ux-item-publisher-link item-banner-focussable-child-item")[1].split(">")[1].split("<")[0]
+    except:
+        author = "Not found"
+    
+    try:    
+        img = data.split("image-display")[1].split('src="')[1].split('"')[0]
+    except:
+        img = "Not found"
+    return render_template('about_one.html', id=ext, name=name, author=author, img=img)
 
 @app.route('/rules')
 def rules():
